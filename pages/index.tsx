cdeb4b72321137cb849396beeb750da42407287e
zemfoliveira <jose.f.oliveira@tecnico.ulisboa.pt>
2023-03-24 14:20:31 +0000

    added ui for login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
-        <title>Netlix Clone</title>
+        <title>Home - Netlix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
